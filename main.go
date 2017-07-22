de92ada5ec9d6f90066235db3741ef6c5f38d446
Tom Scott <tomscott87@gmail.com>
2017-07-22 23:23:07 +0100

    Map now smaller and more like a set

--- main.go
+++ main.go
@@ -8,22 +8,24 @@ import (
 
 func main() {
 
-	dict := make(map[int]map[string][]string)
+	// dict is indexed by the word length then by word
+	// this allows searching by longest word first
+	dict := make(map[int]map[string]struct{})
 
-	dict[3] = make(map[string][]string)
-	test := []string{"POT", "TOP", "OPT"}
-	dict[3]["OPT"] = test
+	dict[3] = make(map[string]struct{})
+	var es struct{}
+	dict[3]["OPT"] = es
+	dict[3]["POT"] = es
+	dict[3]["TOP"] = es
 
 	//fmt.Println(hashWord("POT"))
 	//fmt.Println(hashWord("TOP"))
 
-	guess := "POT"
-	for _, word := range dict[len(guess)][hashWord(guess)] {
-		if word == guess {
-			fmt.Printf("%s is a valid word\n", guess)
-			break
-		}
+	guess := "PTO"
+	if _, ok := dict[len(guess)][guess]; ok {
+		fmt.Printf("%s is a valid word\n", guess)
 	}
+
 }
 
 func hashWord(w string) string {
